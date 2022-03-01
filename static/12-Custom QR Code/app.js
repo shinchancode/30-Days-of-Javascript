6b60a74e2c3a6aa2a0477f08e6a1b2cd7db31895
shinchanncode <shinchanncode@gmail.com>
2022-03-01 20:37:18 +0530

    changes made to the project

--- static/12-Custom QR Code/app.js
+++ static/12-Custom QR Code/app.js
@@ -1,4 +1,4 @@
-const href = "https://aartirathi17.herokuapp.com/";
+const href = "https://shinchancode.github.io/React-Portfolio/";
 const size = 250;
 
 new QRCode(document.querySelector("#qr"), {
